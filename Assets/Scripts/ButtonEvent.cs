b5a3f81f6c8c28f1d46f733a18695607e1e393ea
Oscar Hermansson <oscar.hermansson@learnet.se>
2025-05-26 15:03:03 +0200

    fix knapp

--- Assets/Scripts/ButtonEvent.cs
+++ Assets/Scripts/ButtonEvent.cs
@@ -4,11 +4,17 @@ using UnityEngine.Events;
 public class ButtonEvent : MonoBehaviour, IInteractable
 {
     [SerializeField] private UnityEvent onInteract;
+    [SerializeField] Animator animator;
 
     bool ButtomUp = true;
 
     public void Interact()
     {
+        if (animator != null)
+        {
+            animator.SetTrigger("ButtonPressed");
+        }
+
         if (ButtomUp)
         {
             ButtomUp = false;
