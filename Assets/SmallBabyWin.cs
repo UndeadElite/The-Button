6e916e244faaf32461bac859b9805220c6bc8395
Oscar Hermansson <oscar.hermansson@learnet.se>
2025-05-27 13:15:45 +0200

    calling babywin

--- Assets/SmallBabyWin.cs
+++ Assets/SmallBabyWin.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 
 public class SmallBabyWin : MonoBehaviour
-{  
-    public void OnBabySectionDone()
+{
+    bool BabyKey = false;
+
+    public void Interact()
     {
         Debug.Log("You done baby section!");
+        BabyKey = true;
+        
         // You can add more logic here as needed
     }
 }
