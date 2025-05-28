1918acc90ebe972d511999988b88b7aa2dab62c1
TheUndeadElite <almir.bajraktarevic@elev.ga.lbs.se>
2025-05-28 10:10:13 +0200

    Main menu (not done)

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    public GameObject Button;
+    [SerializeField] Animator animator;
+    public GameObject Explosion;
+    [SerializeField] AudioSource audioSource;
+
+    private void Start()
+    {
+        animator.enabled = true;
+        Explosion.SetActive(false);
+    }
+    public void PlayAnimation()
+    {
+        Debug.Log("playing animation???");
+        if(Button != null)
+        {
+            if(animator != null )
+            {
+                Debug.Log("Playing Animaiton");
+                animator.Play("ButtomDownNew",0);
+            }
+            else
+            {
+                Debug.Log("animator null");
+            }
+        }
+        else
+        {
+            Debug.Log("button null");
+        }
+    }
+
+    public void WaitBeforeChanging()
+    {
+        PlayAnimation();
+        Invoke("SceneChange", 2);
+        Invoke("explode", 1.8f);
+    }
+    void SceneChange()
+    {
+        Debug.Log("Scene Change");
+        SceneManager.LoadScene(1);
+    }
+
+    void explode()
+    {
+        audioSource.Play();
+        Explosion.SetActive(true);
+    }
+}
