e58a50211058ca9c90558681cc81b8f20aaeccb0
Sunnerstai <laa.zoger@gmail.com>
2025-05-28 12:04:45 +0200

    canvasdisabled

--- Assets/PuzzleEscape.cs
+++ Assets/PuzzleEscape.cs
@@ -4,12 +4,14 @@ public class PuzzleEscape : MonoBehaviour
 {
     [SerializeField] GameObject winScreenCanvas;
     [SerializeField] GameObject winText;
+    [SerializeField] GameObject canvas1;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             winScreenCanvas.SetActive(true);
             winText.SetActive(true);
+            canvas1.SetActive(false);
         }
     }
 }
